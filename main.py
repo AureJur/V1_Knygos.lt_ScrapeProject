27df3fa9da2e26e7eca3030aa7cf11a6d0e72d6d
Aurelija <aurelija.jurcenko@gmail.com>
2023-02-12 00:59:35 +0200

    Final code

--- main.py
+++ main.py
@@ -20,37 +20,46 @@ logging.basicConfig(
 )
 
 logging.info("Program execution started")
+
 driver: WebDriver
 with webdriver.Chrome(
     service=ChService('drivers/chromedriver_v109.exe'),
     options=webdriver.ChromeOptions()
 ) as driver:
+    driver.implicitly_wait(2)
     driver.get(config['url_to_scrape'])
-    print(f"Title: {driver.title}")
+    logging.info(f"Scraped URL: {config['url_to_scrape']}")
+    logging.info(f"Title: {driver.title}")
 
     # Press "Accept" button for eu cookie consent
-    time.sleep(2)
     driver.find_element(By.CLASS_NAME, "cc-btn.cc-allow").click()
     logging.info("Pressed 'Accept' button for eu cookie consent")
+
     # NAVIGATION into depth
     # Press "All Books" button
     driver.find_element(By.CSS_SELECTOR, "div.col-auto.menu-item.visos-knygos.mob-view").click()
+    logging.info("Pressed 'All Books' button")
+
     # Hover on "Dalykinė literatūra"
     automationTools = driver.find_element(By.LINK_TEXT, "Dalykinė literatūra")
     ActionChains(driver).move_to_element(automationTools).perform()
+    logging.info("Hovered over 'Dalykinė literatūra'")
+
     # Press "Kompiuterija ir informacinės technologijos" button
     driver.find_element(By.LINK_TEXT, "Kompiuterija ir informacinės technologijos").click()
+    logging.info("Pressed 'Kompiuterija ir informacinės technologijos' button")
     time.sleep(2)
 
     urls, page_numbers = [], []
     book_authors, book_titles, book_prices, book_items = [], [], [], []
 
     # Check if selector exists for author and price
-    def check_exists_selector(selector):
+    def check_element_by_selector(selector):
         try:
             driver.find_element(By.CSS_SELECTOR, selector)
         except NoSuchElementException:
-            logging.warning("Element by such selector is not found")
+            # logging.warning("Element with such selector is not found")
+            logging.warning(f"No element found with selector '{selector}'")
             return False
         return True
 
@@ -58,29 +67,27 @@ with webdriver.Chrome(
     # Number of last_page in "Kompiuterija ir informacinės technologijos"
     all_pages = driver.find_elements(By.XPATH, "//div[2]/div/div[5]/div/ul//a")
     last_page_number = int((all_pages[-2]).text)
+
     # Navigation through pages to get the all_books urls
-    # for k in range(2):
     for k in range(last_page_number):
-        # all_books = driver.find_elements(By.XPATH,
-        #                                  "//*[@id = 'homepage']/div[3]/div[2]/div[3]/div[4]/div/section//a")
         all_books = driver.find_elements(By.CSS_SELECTOR, "a.product-link")
         for book in all_books:
             urls.append(book.get_attribute("href"))
 
-        # go to text page
+        # Go to text page
         next_page_button = driver.find_element(By.CLASS_NAME, "forward")
         next_page_button.click()
 
-    # visit saved pages urls and get titles, authors, prices of the objects (check if author and price exists)
+    # Visit saved pages urls and get titles, authors, prices of the objects (check if author and price exists)
     for url in urls:
         driver.get(url)
-        one_book_authors = []
-        time.sleep(1)
+        # time.sleep(1)
+        one_book_authors: list[str] = []
 
         title = driver.find_element(By.CSS_SELECTOR, "h1 > span:nth-child(1)")
         book_titles.append(title.text)
 
-        if check_exists_selector("div.d-inline-block") > 0:
+        if check_element_by_selector("div.d-inline-block") > 0:
             authors = driver.find_elements(By.CSS_SELECTOR, "div.d-inline-block")
             for author in authors:
                 one_book_authors.append(author.text)
@@ -88,22 +95,19 @@ with webdriver.Chrome(
             one_book_authors.append("N/A")
         book_authors.append(one_book_authors)
 
-        if check_exists_selector("span.new-price.single-price") > 0:
+        if check_element_by_selector("span.new-price.single-price") > 0:
             price = driver.find_element(By.CSS_SELECTOR, "span.new-price.single-price")
             book_prices.append(price.text)
-        elif check_exists_selector("span.price.single-price") > 0:
+        elif check_element_by_selector("span.price.single-price") > 0:
             price = driver.find_element(By.CSS_SELECTOR, "span.price.single-price")
             book_prices.append(price.text)
-        elif check_exists_selector("ul.prices.mb-4 > li > span") > 0:
+        elif check_element_by_selector("ul.prices.mb-4 > li > span") > 0:
             price = driver.find_element(By.CSS_SELECTOR, "ul.prices.mb-4 > li > span")
             book_prices.append(price.text)
         else:
             book_prices.append("N/A €")
 
-    author_strings = []
-    for author_list in book_authors:
-        author_string = " ".join(author_list)
-        author_strings.append(author_string)
+    author_strings = [" ".join(author_list) for author_list in book_authors]
 
     with open("report/v1_books.csv", "w", encoding='UTF-8', newline="") as f:
         writer = csv.writer(f, delimiter=";")
--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@
 ### Launch procedure
 - Launch command line
 - Issue the command: 'git clone https://github.com/AureJur/V1_Knygos.lt_ScrapeProject.git'
+- Open in favorite editor / IDE and configure virtual environment with Python v.10 
 - Install the necessary libraries: 'pip install -r requirements.txt'
-- Open in favorite editor / IDE and configure virtual environment
